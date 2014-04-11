d085d7e3a882d12b4bda1f1e2be8ac042980eb88
Nils Wisiol <info@nils-wisiol.de>
2014-04-11 13:35:12 -0400

    feat(stops): added north campus shuttle stops

--- datasource.js
+++ datasource.js
@@ -23,7 +23,30 @@ var data = {};
 /**
  * Data served by this server that is not expected to change
  */
-var staticdata = {};
+var staticdata = {
+  stop: [
+    { name: 'Creekside North', latitude: 43.011410, longitude: -78.792681 },
+    { name: 'Creekside South', latitude: 43.010907, longitude: -78.790595 },
+    { name: 'Ellicot Tunnel', latitude: 43.008193, longitude: -78.785898 },
+    { name: 'South Lake (to Spine)', latitude: 43.003391, longitude: -78.7782870 },
+    { name: 'Alumni/Stadium (to Spine)', latitude: 43.000213, longitude: -78.780025 },
+    { name: 'Center for the Arts', latitude: 43.000245, longitude: -78.782852 },
+    { name: 'Lockwood (to Spine)', latitude: 42.999825, longitude: -78.785197 },
+    { name: 'Baldy/O\'Brian', latitude: 43.000170, longitude: -78.787493 },
+    { name: 'Founder\'s Plaza', latitude: 43.000346, longitude: -78.788942 },
+    { name: 'Cooke / Hochstetter', latitude: 42.999519, longitude: -78.791130 },
+    { name: 'Natural Sciences Complex', latitude: 43.000076, longitude: -78.792584 },
+    { name: 'Flickinger Court', latitude: 43.005211, longitude: -78.800545 },
+    { name: 'Hadley Village', latitude: 42.998587, longitude: -78.794888 },
+    { name: 'Computing Center', latitude: 43.001437, longitude: -78.792442 },  
+    { name: 'Lower Capen', latitude: 43.001441, longitude: -78.789647 },
+    { name: 'Student Union', latitude: 43.001724, longitude: -78.786203 },
+    { name: 'Lockwood (to Ellicot)', latitude: 43.001055, longitude: -78.785380 },
+    { name: 'Alumni/Stadium (to Ellicot)', latitude: 43.000213, longitude: -78.780025 },
+    { name: 'South Lake (to Ellicot)', latitude: 43.002320, longitude: -78.776495 },
+    { name: 'Greiner Hall', latitude: 43.006698, longitude: -78.785858 },
+  ],
+};
 
 /**
  * The current status of the server.
@@ -84,7 +107,8 @@ function parseResponse(body) {
   data.bus = find('AddBus', body, ['id', 'title', 'address', 'latitude', 'longitude', 'iconColor', 'direction', 'speed', 'timestamps', 'lastStop', 'route']);
   
   // get the data that is expected to stay the same
-  data.stop = find('AddStop', body, ['title', 'longitude', 'latitude', 'icon', 'route']);
+  // screw that, there is no data provided by this source anyways.
+  //data.stop = find('AddStop', body, ['title', 'longitude', 'latitude', 'icon', 'route']);
 
   var regex = /ResetRoutePoints\(\);(AddRoutePoint\(-?[0-9]*\.[0-9]*, -?[0-9]*\.[0-9]*\);)*AddRouteToMap\([0-9]*, [0-9]*, [0-9]*, [0-9]*\);/g;
   (body.match(regex) || []).forEach(function(r) {
@@ -122,7 +146,6 @@ function updateData(callback) {
       var body = Buffer.concat(bodyChunks).toString();
       var parsed = parseResponse(body);
       data.bus = parsed.bus;
-      staticdata.stop = parsed.stop;
       staticdata.route = parsed.route;
       status = 200;
       callback(true);
